928570899e95f82b5f0b5a52db1189a2ab993230
shivsut <100508768+shivsut@users.noreply.github.com>
2024-04-03 16:51:08 +0530

    update reward

--- state_agent/reward.py
+++ state_agent/reward.py
@@ -3,8 +3,10 @@ class Reward:
     def __init__(self):
         self.prev_reward = 0
     def step(self, p_states):
-        curr_reward = p_states[0]
+        curr_reward = -p_states[0]
         if len(p_states) > 1:
-            curr_reward += p_states[1]
+            curr_reward += -p_states[1]
+        curr_reward *= 0.5
+        curr_reward += curr_reward - self.prev_reward
         self.prev_reward = curr_reward
-        return -curr_reward * 0.5
+        return curr_reward
