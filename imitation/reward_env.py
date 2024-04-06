24585e7b2e1e548f47afe676c1382d9d37dc4898
shivsut <shiva.shankara@utexas.edu>
2024-04-06 14:18:03 +0530

    updates

--- imitation/reward_env.py
+++ imitation/reward_env.py
@@ -6,6 +6,8 @@ from gymnasium import spaces
 import logging
 import numpy as np
 from collections import namedtuple
+
+from state_agent.extract_state import extract_state_train
 from ..tournament.utils import VideoRecorder
 
 TRACK_NAME = 'icy_soccer_field'
@@ -39,8 +41,25 @@ def to_native(o):
             return {k: _to(getattr(v, k)) for k in dir(v) if k[0] != '_'}
     return _to(o)
 
+class DummyTeam():
+    def __init__(self, num_players, team_id):
+        self.num_players = num_players
+        self.team_id = team_id
+    def new_match(self):
+        return ['tux'] * self.num_players
+    def act(self, action):
+        # return [dict(acceleration=action[0], steer=action[1], brake=True if action[2] > 0.05 else False, nitro=True if action[3] > 0.05 else False, drift=True if action[4] > 0.05 else False, rescue=False, fire=False)]
+        return [dict(acceleration=action[0], steer=action[1], brake=False, nitro=False, drift=False, rescue=False, fire=False)]
+        # return [dict(acceleration=action[0]/10, steer=action[1]/100, brake=True if action[2] >0 else False, nitro=True if action[3] >0 else False, drift=True if action[4] >0 else False, rescue=False, fire=False)]
+    def reset(self):
+        pass
+
 class IceHockeyEnvImitation(gymnasium.Env):
-    def __init__(self):
+    def __init__(self, args, logging_level=None):
+        self.do_init = True
+        self.args = args
+        self.logging_level = logging_level
+        self.init()
         pi = 3.2
         super(IceHockeyEnvImitation, self).__init__()
         #
@@ -57,17 +76,98 @@ class IceHockeyEnvImitation(gymnasium.Env):
         # goal_line_center - our  goal y
         # puck_to_goal_line_angle - -pi to pi
         # kart_to_puck_angle_difference - - 1 to 1
+
         # kart_to_opponent0_angle - -pi to pi
         # kart_to_opponent1_angle - -pi to pi
         # kart_to_opponent0_angle_difference - -1 to 1
         # kart_to_opponent1_angle_difference - -1 to 1
         self.observation_space = spaces.Box(low=np.array([0, 0, -pi, -pi, 0, 0, 0, 0, 0, 0, -pi, -1, -pi, -pi, -1, -1]), high=np.array([100, 100, pi, pi, 100, 100, 100, 100, 60, 60, -pi, 1, pi, pi, 1, 1]),dtype=np.float32)
 
-    def step(self, action):
-        raise Exception("Not Implemented")
 
-    def reset(self,seed=1, options=None):
-        raise Exception("Not Implemented")
+        self._pystk = pystk
+        self._pystk.init(self._pystk.GraphicsConfig.none())
+        if self.logging_level is not None:
+            logging.basicConfig(level=self.logging_level)
+        self.recorder = VideoRecorder(self.args.record_fn) if self.args.record_fn else None
+
+        # self.team1 = AIRunner() if kwargs['team1'] == 'AI' else TeamRunner("")
+        # self.team2 = AIRunner() if kwargs['team2'] == 'AI' else TeamRunner("")
+        self.timeout = 1e10
+        self.max_score = 3
+        self.num_players = 1
+        self.team1 = DummyTeam(self.num_players, 0)
+
+        self.info = {}
+        self.race_config = self._pystk.RaceConfig(track=TRACK_NAME, mode=self._pystk.RaceConfig.RaceMode.SOCCER, num_kart=1 * self.num_players)
+        self.race_config.players.pop()
+        for i in range(self.num_players):
+            # self.race_config.players.append(self._make_config(0, hasattr(self.team1, 'is_ai') and self.team1.is_ai, ['tux']))
+            self.race_config.players.append(self._make_config(0, False, 'tux'))
+        # self.reset()
+        self.race = self._pystk.Race(self.race_config)
+
+    def _make_config(self, team_id, is_ai, kart):
+        PlayerConfig = self._pystk.PlayerConfig
+        controller = PlayerConfig.Controller.AI_CONTROL if is_ai else PlayerConfig.Controller.PLAYER_CONTROL
+        return PlayerConfig(controller=controller, team=team_id, kart=kart)
+
+    def step(self, action):
+        #pystk
+        team1_state = [to_native(p) for p in self.state.players[0::2]]
+        team2_state = [to_native(p) for p in self.state.players[1::2]]
+        soccer_state = to_native(self.state.soccer)
+        logging.info('calling agent')
+        team1_actions = self.team1.act(action)
+        # team2_actions = self.team2.act(team2_state, team1_state, soccer_state)
+
+        # TODO check for error in info and raise MatchException
+        # TODO check for timeout
+
+        if self.recorder:
+            self.recorder(team1_state, team2_state, soccer_state=soccer_state, actions=team1_actions,team1_images=None, team2_images=None)
+
+        if (not self.race.step([self._pystk.Action(**a) for a in team1_actions]) and self.num_players):
+            self.truncated = True
+        if (sum(self.state.soccer.score) >= self.max_score):
+            self.terminated = True
+        if not (self.truncated or self.terminated):
+            self.state.update()
+
+        logging.info('state updated, calculating reward')
+        team1_state_next = [to_native(p) for p in self.state.players[0::2]]
+        team2_state_next = [to_native(p) for p in self.state.players[1::2]]
+        soccer_state = to_native(self.state.soccer)
+        p_features = extract_state_train(team1_state_next, team2_state_next, soccer_state, 0)
+
+        # reward = self.reward.step(p_features)
+        # logging.info(f'returning new state and reward {reward}')
+        # print(f"reward: {reward}")
+        return np.array(p_features), 0, self.terminated or self.truncated, self.info
+
+    def reset(self, seed=1, options=None):
+        # super().reset(seed=seed)
+        logging.info('Resetting')
+        # self.recorder = VideoRecorder('infer.mp4') if self.args.record_fn else None
+        # self.reward = Reward()
+        self.truncated = False
+        self.terminated = False
+        logging.info('Starting new race')
+        self.race.stop()
+        self.race.start()
+        self.state = self._pystk.WorldState()
+        self.state.update() # TODO need to call this here?
+
+        team1_state_next = [to_native(p) for p in self.state.players[0::2]]
+        team2_state_next = [to_native(p) for p in self.state.players[1::2]]
+        soccer_state = to_native(self.state.soccer)
+        p_features = extract_state_train(team1_state_next, team2_state_next, soccer_state, 0)
+        # print(p_features)
+        return np.array(p_features), self.info
+
+    def close(self):
+        self.race.stop()
+        del self.race
+        self._pystk.clean()
 class IceHockeyEnvImitation2(gymnasium.Env):
     """
     Observation:
--- state_agent/extract_state.py
+++ state_agent/extract_state.py
@@ -47,13 +47,54 @@ def extract_state_train_players(p1_state, soccer_state, opponent_state, team_id,
 
     return output
 
-def extract_state_train(p_states, opponent_state, soccer_state, team_id):
+def extract_state_train1(p_states, opponent_state, soccer_state, team_id):
     res = []
     res.extend(extract_state_train_players(p_states[0], soccer_state, opponent_state, team_id))
     if len(p_states) > 1:
         res.extend(extract_state_train_players(p_states[1], soccer_state, opponent_state, team_id, player_type='defense'))
     return res
 
+def extract_state_train(p_states, opponent_state, soccer_state, team_id):
+    # features of ego-vehicle
+    kart_front = torch.tensor(p_states['kart']['front'], dtype=torch.float32)[[0, 2]]
+    kart_center = torch.tensor(p_states['kart']['location'], dtype=torch.float32)[[0, 2]]
+    kart_direction = (kart_front-kart_center) / torch.norm(kart_front-kart_center)
+    kart_angle = torch.atan2(kart_direction[1], kart_direction[0])
+
+    # features of soccer
+    puck_center = torch.tensor(soccer_state['ball']['location'], dtype=torch.float32)[[0, 2]]
+    kart_to_puck_direction = (puck_center - kart_center) / torch.norm(puck_center-kart_center)
+    kart_to_puck_angle = torch.atan2(kart_to_puck_direction[1], kart_to_puck_direction[0])
+
+    kart_to_puck_angle_difference = limit_period((kart_angle - kart_to_puck_angle)/np.pi)
+
+    # features of opponents
+    opponent_center0 = torch.tensor(opponent_state[0]['kart']['location'], dtype=torch.float32)[[0, 2]]
+    opponent_center1 = torch.tensor(opponent_state[1]['kart']['location'], dtype=torch.float32)[[0, 2]]
+
+    kart_to_opponent0 = (opponent_center0 - kart_center) / torch.norm(opponent_center0-kart_center)
+    kart_to_opponent1 = (opponent_center1 - kart_center) / torch.norm(opponent_center1-kart_center)
+
+    kart_to_opponent0_angle = torch.atan2(kart_to_opponent0[1], kart_to_opponent0[0])
+    kart_to_opponent1_angle = torch.atan2(kart_to_opponent1[1], kart_to_opponent1[0])
+
+    kart_to_opponent0_angle_difference = limit_period((kart_angle - kart_to_opponent0_angle)/np.pi)
+    kart_to_opponent1_angle_difference = limit_period((kart_angle - kart_to_opponent1_angle)/np.pi)
+
+    # features of score-line
+    goal_line_center = torch.tensor(soccer_state['goal_line'][team_id], dtype=torch.float32)[:, [0, 2]].mean(dim=0)
+
+    puck_to_goal_line = (goal_line_center-puck_center) / torch.norm(goal_line_center-puck_center)
+    puck_to_goal_line_angle = torch.atan2(puck_to_goal_line[1], puck_to_goal_line[0])
+    kart_to_goal_line_angle_difference = limit_period((kart_angle - puck_to_goal_line_angle)/np.pi)
+
+    features = torch.tensor([kart_center[0], kart_center[1], kart_angle, kart_to_puck_angle, opponent_center0[0],
+        opponent_center0[1], opponent_center1[0], opponent_center1[1], kart_to_opponent0_angle, kart_to_opponent1_angle,
+        goal_line_center[0], goal_line_center[1], puck_to_goal_line_angle, kart_to_puck_angle_difference,
+        kart_to_opponent0_angle_difference, kart_to_opponent1_angle_difference,
+        kart_to_goal_line_angle_difference], dtype=torch.float32)
+
+    return features
 def extract_state_infer(p_states, soccer_state, opponent_state, team_id):
     res = []
     res.extend(extract_state_train_players(p_states[0], soccer_state, opponent_state, team_id))
