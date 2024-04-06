e6a3ef2485257d53366399937389f55eb55092d8
SundayAI2 <165758779+SundayAI2@users.noreply.github.com>
2024-04-06 15:14:41 +0530

    Update policy_env.py

--- imitation/policy_env.py
+++ imitation/policy_env.py
@@ -30,46 +30,46 @@ class IceHockeyEnv(BasePolicy):
         self.team = team
         self.model = torch.jit.load(path.join(path.dirname(path.abspath(__file__)), 'jurgen_agent.pt'))
         
-    def extract_featuresV2(self, pstate, soccer_state, opponent_state, team_id):
-        def limit_period(angle):
-            # turn angle into -1 to 1 
-            return angle - torch.floor(angle / 2 + 0.5) * 2 
+    # def extract_featuresV2(self, pstate, soccer_state, opponent_state, team_id):
+    #     def limit_period(angle):
+    #         # turn angle into -1 to 1 
+    #         return angle - torch.floor(angle / 2 + 0.5) * 2 
         
-        # features of ego-vehicle
-        kart_front = torch.tensor(pstate['kart']['front'], dtype=torch.float32)[[0, 2]]
-        kart_center = torch.tensor(pstate['kart']['location'], dtype=torch.float32)[[0, 2]]
-        kart_direction = (kart_front-kart_center) / torch.norm(kart_front-kart_center)
-        kart_angle = torch.atan2(kart_direction[1], kart_direction[0])
+    #     # features of ego-vehicle
+    #     kart_front = torch.tensor(pstate['kart']['front'], dtype=torch.float32)[[0, 2]]
+    #     kart_center = torch.tensor(pstate['kart']['location'], dtype=torch.float32)[[0, 2]]
+    #     kart_direction = (kart_front-kart_center) / torch.norm(kart_front-kart_center)
+    #     kart_angle = torch.atan2(kart_direction[1], kart_direction[0])
 
-        # features of soccer 
-        puck_center = torch.tensor(soccer_state['ball']['location'], dtype=torch.float32)[[0, 2]]
-        kart_to_puck_direction = (puck_center - kart_center) / torch.norm(puck_center-kart_center)
-        kart_to_puck_angle = torch.atan2(kart_to_puck_direction[1], kart_to_puck_direction[0]) 
+    #     # features of soccer 
+    #     puck_center = torch.tensor(soccer_state['ball']['location'], dtype=torch.float32)[[0, 2]]
+    #     kart_to_puck_direction = (puck_center - kart_center) / torch.norm(puck_center-kart_center)
+    #     kart_to_puck_angle = torch.atan2(kart_to_puck_direction[1], kart_to_puck_direction[0]) 
 
-        kart_to_puck_angle_difference = limit_period((kart_angle - kart_to_puck_angle)/np.pi)
+    #     kart_to_puck_angle_difference = limit_period((kart_angle - kart_to_puck_angle)/np.pi)
 
-        # features of score-line 
-        goal_line_center = torch.tensor(soccer_state['goal_line'][(team_id+1)%2], dtype=torch.float32)[:, [0, 2]].mean(dim=0)
+    #     # features of score-line 
+    #     goal_line_center = torch.tensor(soccer_state['goal_line'][(team_id+1)%2], dtype=torch.float32)[:, [0, 2]].mean(dim=0)
 
-        puck_to_goal_line = (goal_line_center-puck_center) / torch.norm(goal_line_center-puck_center)
+    #     puck_to_goal_line = (goal_line_center-puck_center) / torch.norm(goal_line_center-puck_center)
 
-        features = torch.tensor([kart_center[0], kart_center[1], kart_angle, kart_to_puck_angle, 
-            goal_line_center[0], goal_line_center[1], kart_to_puck_angle_difference, 
-            puck_center[0], puck_center[1], puck_to_goal_line[0], puck_to_goal_line[1]], dtype=torch.float32)
+    #     features = torch.tensor([kart_center[0], kart_center[1], kart_angle, kart_to_puck_angle, 
+    #         goal_line_center[0], goal_line_center[1], kart_to_puck_angle_difference, 
+    #         puck_center[0], puck_center[1], puck_to_goal_line[0], puck_to_goal_line[1]], dtype=torch.float32)
 
-        return features 
+    #     return features 
 
-    def act(self, player_state, opponent_state, soccer_state):
-        actions = [] 
-        for player_id, pstate in enumerate(player_state):
-            features = self.extract_featuresV2(pstate, soccer_state, opponent_state, self.team)
-            acceleration, steer, brake = self.model(features)
-            actions.append(dict(acceleration=acceleration, steer=steer, brake=brake))                        
-        return actions 
+    # def act(self, player_state, opponent_state, soccer_state):
+    #     actions = [] 
+    #     for player_id, pstate in enumerate(player_state):
+    #         features = self.extract_featuresV2(pstate, soccer_state, opponent_state, self.team)
+    #         acceleration, steer, brake = self.model(features)
+    #         actions.append(dict(acceleration=acceleration, steer=steer, brake=brake))                        
+    #     return actions 
 
-    def obs_convert(self):
+    # def obs_convert(self):
 
-        pass 
+    #     pass 
 
     def predict(
         self,
@@ -79,9 +79,10 @@ class IceHockeyEnv(BasePolicy):
         deterministic: bool = False,
     ) -> Tuple[np.ndarray, Optional[Tuple[np.ndarray, ...]]]:
 
-        player_state, opponent_state, soccer_state = self.obs_convert(observation)
-        actions = self.act(player_state, opponent_state, soccer_state)
-        return actions, state 
+        # player_state, opponent_state, soccer_state = self.obs_convert(observation)
+        # actions = self.act(player_state, opponent_state, soccer_state)
+        # return actions, state 
+        return self.model(observation), state
         
 
 
