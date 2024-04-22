626936446b2cdec5ad4779b60a90417c950b57fe
shivsut <100508768+shivsut@users.noreply.github.com>
2024-04-22 19:05:34 +0530

    Update player.py

--- state_agent/player.py
+++ state_agent/player.py
@@ -23,6 +23,8 @@ class Team:
 
         team_blue = "/dl_fp_main/AI_L2x256_blue/AI_L2x256_blue_jit.pt"
         team_red = "/dl_fp_main/AI_L2x256_red/AI_L2x256_red_jit.pt"
+        team_blue = 'grader_blue/10_blue.pt'
+        team_red = 'grader_blue/red.pt'
         self.model_blue = torch.jit.load(team_blue, map_location=self.device)
         self.model_red = torch.jit.load(team_red, map_location=self.device)
         self.running_avg = []
@@ -81,23 +83,18 @@ class Team:
         """
 
         actions = []
-        start = time.time()
+        # start = time.time()
         for player_id, pstate in enumerate(player_state):
             # TODO: Use Policy to get the actions of each player
-            if self.use_model:
-                features = extract_featuresV2(pstate, opponent_state, soccer_state, self.team)
-                data = features.clone().detach().to(self.device)
-                action = self.model(data)
-                action = dict(acceleration=action[0], steer=action[1], brake=action[2])
-            else:
-                # Generate random actions for all players
-                action = dict(acceleration=0.7, brake=0, drift=0,
-                              fire=0, nitro=0, rescue=0, steer=0.3)
-            
+            # if self.use_model:
+            features = extract_featuresV2(pstate, opponent_state, soccer_state, self.team)
+            data = features.clone().detach().to(self.device)
+            action = self.model(data)
+            action = dict(acceleration=action[0], steer=action[1], brake=action[2])
             # accumulate the action of each player
             actions.append(action)
-        end = time.time()
-        self.running_avg.append(end-start)
+        # end = time.time()
+        # self.running_avg.append(end-start)
         # if len(self.running_avg) > 20:
         #     print(f"avg act time {np.mean(self.running_avg)*1000} ms")
         #     self.running_avg = []
